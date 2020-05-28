e1950b4c7510a4342a25e49c2cb36d73903b3210
HhTtLllL <1430249706@qq.com>
2020-05-29 04:35:31 +0800

    Menu

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -7,10 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Login w;
-    //w.show();
-    Mainmenu w1;
-    w1.show();
-
+    w.show();
 
     return a.exec();
 }
--- untitled/mainmenu.cpp
+++ untitled/mainmenu.cpp
@@ -13,16 +13,59 @@ Mainmenu::Mainmenu(QWidget *parent) :
     ui->setupUi(this);
 
     connect(ui->modify_password,&QAction::triggered,[=]()
-
     {
-        qDebug() << "修改密码" ;
+
+        //切换界面，切换至 修改密码的界面
+        ui->stackedWidget->setCurrentWidget(ui->modify_password_ui);
+        //ui->stackedWidget->setCurrentWidget(ui->modifi_password_ui);
     });
-    /*connect(ui->modify_password,&QPushButton::clicked,[=]()
 
+    connect(ui->modify_information,&QAction::triggered,[=]()
     {
-        qDebug() << "修改密码" ;
-    });*/
+        ui->stackedWidget->setCurrentWidget(ui->modify_information_ui);
+    });
 
+    connect(ui->logout,&QAction::triggered,[=]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->logout_ui);
+
+    });
+
+    connect(ui->delete_movie,&QAction::triggered,[=]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->delete_movie_ui);
+
+    });
+
+    connect(ui->add_movie,&QAction::triggered,[=]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->add_movie_ui);
+    });
+
+    connect(ui->modify_movie,&QAction::triggered,[=]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->modify_movie_inforation);
+    });
+
+    connect(ui->add_theater,&QAction::triggered,[=]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->add_theater_ui);
+    });
+
+    connect(ui->delete_theater,&QAction::triggered,[=]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->delete_theater_ui);
+    });
+
+    connect(ui->modify_theater,&QAction::triggered,[=]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->modify_theater_ui);
+    });
+
+    connect(ui->add_movie_plan,&QAction::triggered,[=]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->add_movie_plan_ui);
+    });
 }
 
 Mainmenu::~Mainmenu()
